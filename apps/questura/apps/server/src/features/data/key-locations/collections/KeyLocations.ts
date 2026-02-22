ef580677398d98e2b505ae1dd28c0c6eadab9b37
alantothe <rubenamalpartida@gmail.com>
2026-02-22 15:45:04 -0500

    fix key-locations schema syntax in tab field blocks

--- apps/questura/apps/server/src/features/data/key-locations/collections/KeyLocations.ts
+++ apps/questura/apps/server/src/features/data/key-locations/collections/KeyLocations.ts
@@ -192,7 +192,7 @@ export const KeyLocations: CollectionConfig = {
                     },
                   },
                 },
-              },
+              ],
             },
           ],
         },
