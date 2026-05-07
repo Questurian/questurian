aead51f4a77ca4c2375a47ca52cadb09c94e9947
alantothe <rubenamalpartida@gmail.com>
2026-05-07 13:38:23 -0400

    map page- nav bar

--- apps/questura/apps/client/src/app/(main)/layout.tsx
+++ apps/questura/apps/client/src/app/(main)/layout.tsx
@@ -11,7 +11,7 @@ export default function MainLayout({
   return (
     <>
       <Navbar />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 ">{children}</main>
       <Footer />
     </>
   );
--- apps/questura/apps/client/src/features/articles/layouts/MapsArticleLayout.tsx
+++ apps/questura/apps/client/src/features/articles/layouts/MapsArticleLayout.tsx
@@ -35,7 +35,6 @@ export function MapsArticleLayout({
       <div
         data-article-layout="maps"
         className="1024:flex 1024:min-h-screen 1024:max-w-[1600px] 1024:mx-auto"
-        style={{ marginTop: "calc((1 - var(--navbar-collapse, 0)) * 20px)" }}
       >
         <div className="1024:flex-1 1024:min-w-0 1024:border-r 1024:border-foreground/10">
           <MapsListicleArticlePage article={article} />
